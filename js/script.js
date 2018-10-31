ecfa5737a51bc2c6c4468cfc37f3deb1a4f10082
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2018-10-31 20:43:28 +0200

    :rainbow: Added jQuery slideshow

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Lightbox
+lightbox.option({
+'fitImagesInVieport':true,
+ 'maxHeight': 1000,
+ 'maxWidth': 1000,
+ 'wraparound': true,
+ 'positionFromTop':200  ,
+ 'alwaysShowNavOnTouchDevices':true
+})
+
+/*
+lightBox.load(function() {       
+    $("a").lightBox();
+});*/
