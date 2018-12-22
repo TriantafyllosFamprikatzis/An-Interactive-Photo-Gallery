c4b92c0d46db38fcf91f51df597794a204244a7c
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2018-12-23 00:08:11 +0200

    JavaScript performance updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //LIGHTBOX
 lightbox.option({
-'fitImagesInVieport':true,
- 'disableScrolling':true,
- 'positionFromTop':100
+    'fitImagesInVieport': true,
+    'disableScrolling': true,
+    'positionFromTop': 100
 })
 
 
@@ -14,15 +14,15 @@ function myFunction() {
     let filter = input.value.toLowerCase();
     let images = document.getElementsByClassName('thumb');
 
-// Loop through all list items, and hide those who don't match the search query
-    for (let i = 0; i < images.length; i +=1) {
-       let getInfo = images[i].getElementsByTagName("a")[0];
+    // Loop through all list items, and hide those who don't match the search query
+    for (let i = 0; i < images.length; i += 1) {
+        let getInfo = images[i].getElementsByTagName('a')[0];
         if (getInfo.innerHTML.toLowerCase().indexOf(filter) > -1) {
-            images[i].style.display = "";
+            images[i].style.display = '';
         } else {
-            images[i].style.display = "none";
+            images[i].style.display = 'none';
         }
     }
 }
 //Call the function by listening the Event when Keyup
-input.addEventListener("keyup", myFunction);
+input.addEventListener('keyup', myFunction);
