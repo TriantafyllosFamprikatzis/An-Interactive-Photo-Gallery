3592427effeef7fc327282efc29499a120a15df3
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2018-12-22 23:29:59 +0200

    JavaScript performance updates

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,16 @@ lightbox.option({
 
 
 //SEARCH BAR
+
 const input = document.getElementById('image-search');
 
 function myFunction() {
-// Declare variables
-    let filter, images, getInfo, i;
-    filter = input.value.toLowerCase();
-    images = document.getElementsByClassName('thumb');
+    let filter = input.value.toLowerCase();
+    let images = document.getElementsByClassName('thumb');
 
 // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < images.length; i++) {
-        getInfo = images[i].getElementsByTagName("a")[0];
+    for (let i = 0; i < images.length; i +=1) {
+       let getInfo = images[i].getElementsByTagName("a")[0];
         if (getInfo.innerHTML.toLowerCase().indexOf(filter) > -1) {
             images[i].style.display = "";
         } else {
